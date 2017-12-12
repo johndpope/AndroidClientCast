d88c9af956498d3c04fdf482a6b9e999c0552014
João Coelho <admin.henyah@gmail.com>
2017-12-12 16:37:26 +0100

    Track names.

--- src/main/java/net/ericsson/emovs/cast/ui/TrackSelectorFragment.java
+++ src/main/java/net/ericsson/emovs/cast/ui/TrackSelectorFragment.java
@@ -6,7 +6,13 @@ import android.support.v4.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
 
+import net.ericsson.emovs.cast.EMPCastProvider;
+import net.ericsson.emovs.cast.EmpReceiverChannel;
+import net.ericsson.emovs.cast.models.MediaTrack;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Locale;
 
 /**
  * Created by Joao Coelho on 2017-12-11.
@@ -15,13 +21,11 @@ import java.util.ArrayList;
 public class TrackSelectorFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // TODO: refactor
         final ArrayList mSelectedItems = new ArrayList();
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        CharSequence[] tackCodes = { "en", "es", "pt" };
 
         builder.setTitle("Track Selection")
-                .setMultiChoiceItems(tackCodes, null,
+                .setMultiChoiceItems(getTrackNames(), null,
                         new DialogInterface.OnMultiChoiceClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which, boolean isChecked) {
@@ -42,5 +46,26 @@ public class TrackSelectorFragment extends DialogFragment {
                 });
 
         return builder.create();
+    }
+
+    private CharSequence[] getTrackNames() {
+        EmpReceiverChannel empReceiverChannel = EMPCastProvider.getInstance().getReceiverChannel();
+        ArrayList<CharSequence> trackNames = new ArrayList<>();
+
+        if (empReceiverChannel.audioTracks != null) {
+            for (MediaTrack track : empReceiverChannel.audioTracks) {
+                Locale locale = new Locale(track.getLanguage());
+                trackNames.add("(audio) " + locale.getDisplayLanguage().substring(0, 1).toUpperCase() + locale.getDisplayLanguage().substring(1));
+            }
+        }
+
+        if (empReceiverChannel.textTracks != null) {
+            for (MediaTrack track : empReceiverChannel.textTracks) {
+                Locale locale = new Locale(track.getLanguage());
+                trackNames.add("(subs) " + locale.getDisplayLanguage().substring(0, 1).toUpperCase() + locale.getDisplayLanguage().substring(1));
+            }
+        }
+
+        return trackNames.toArray(new CharSequence[trackNames.size()]);
     }
 }
